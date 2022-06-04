329d685b14259f0968074bb4407b3f640b1bec2e
Yogendra <yogendrasai02@gmail.com>
2022-06-04 22:47:59 +0530

    Initial starter project commit

---
+++ src/test/java/com/techion/techion/TechIonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techion.techion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechIonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
